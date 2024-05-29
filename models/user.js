12f3449cbb67749b3528aa1db25e7c6037afc92b
Miguel Bandeira <61510901+miguelgbandeira@users.noreply.github.com>
2024-05-29 16:07:21 +0100

    Add user schema

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema({
+  first_name: { type: String, required: true, maxLength: 100 },
+  last_name: { type: String, required: true, maxLength: 100 },
+  username: { type: String, required: true, maxLength: 100 },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  membership: { type: String },
+  isAdmin: { type: Boolean },
+});
+
+UserSchema.virtual("name").get(function () {
+  let fullname = "";
+  if (this.first_name && this.family_name) {
+    fullname = `${this.first_name}, ${this.last_name}`;
+  }
+  return fullname;
+});
+
+module.exports = mongoose.model("User", UserSchema);
