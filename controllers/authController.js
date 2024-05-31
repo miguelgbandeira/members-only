f44086b2e51a2c9f46617bf4a4e0cf5f1d2ca0d6
Miguel Bandeira <61510901+miguelgbandeira@users.noreply.github.com>
2024-05-31 15:35:48 +0100

    Add form sanitization to login and sign up

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,37 +1,88 @@
 const passport = require("passport");
 const User = require("../models/user");
 const { createHash } = require("../middleware/auth");
+const { body, validationResult } = require("express-validator");
 
 exports.get_signup = async (req, res, next) => {
-  res.render("sign-up-form");
+  res.render("sign-up-form", { errors: [] });
 };
 
-exports.submit_signup = async (req, res, next) => {
-  try {
-    const hashedPassword = await createHash(req.body.password);
-    const user = new User({
-      first_name: req.body.firstName,
-      last_name: req.body.lastName,
-      username: req.body.username,
-      email: req.body.email ? req.body.email : undefined,
-      password: hashedPassword,
-    });
-    const result = await user.save();
-    res.redirect("/");
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
-};
+exports.submit_signup = [
+  body("firstName")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified.")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+  body("lastName")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Last name must be specified.")
+    .isAlphanumeric()
+    .withMessage("Last name has non-alphanumeric characters."),
+  body("last_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Username must be specified."),
+
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.render("sign-up-form", { errors: errors.array() });
+      }
+      const hashedPassword = await createHash(req.body.password);
+      const user = new User({
+        first_name: req.body.firstName,
+        last_name: req.body.lastName,
+        username: req.body.username,
+        email: req.body.email ? req.body.email : undefined,
+        password: hashedPassword,
+      });
+      const result = await user.save();
+      res.redirect("/");
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  },
+];
 
 exports.get_login = async (req, res, next) => {
-  res.render("log-in-form");
+  res.render("log-in-form", { errors: [] });
 };
 
-exports.submit_login = passport.authenticate("local", {
-  successRedirect: "/",
-  failureRedirect: "/auth/log-in",
-});
+exports.submit_login = [
+  // Validate and sanitize fields
+  body("username")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Username must be specified.")
+    .isAlphanumeric()
+    .withMessage("Username has non-alphanumeric characters."),
+  body("password")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Password must be specified."),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.render("log-in-form", { errors: errors.array() });
+    }
+    next();
+  },
+
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/auth/log-in",
+  }),
+];
 
 exports.get_logout = (req, res, next) => {
   req.logout((err) => {
